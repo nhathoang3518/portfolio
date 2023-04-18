cfb1e50998073a662940a18ef5d78c31dbde5251
nhathoang3518 <129054487+nhathoang3518@users.noreply.github.com>
2023-04-19 01:59:04 +0700

    Updates

---
+++ components/About/AboutCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Nguyen Nhat Hoang</span>
+            <br />
+            <span>
+              I graduated from Vietnam - Korea University of Information and
+              Communication Technology.
+            </span>
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Badminton
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Read Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
